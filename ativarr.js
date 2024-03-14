da1b739c843495d2ddddb15279b007ee5113d91d
MatheusSC <matheuscouto.tds2.senai>
2024-03-14 10:49:09 -0300

    reverse

---
+++ ativarr.js
@@ -0,0 +1,11 @@
+//criando a lista de cores
+let listaDeCores = ['1-azul', '2-vermelho', '3-rosa', '4-verde'];
+
+//exibindo a lista
+console.log("-_-_-Lista de cores-_-_-");
+console.log(listaDeCores);
+
+//reverse
+listaDeCores.reverse()
+console.log("-_-_-Lista revertida-_-_-");
+console.log(listaDeCores);
